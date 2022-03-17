81e6cc3bab58abdf71a029415fac87aa2ac0ed17
KNChiu <y9760210@gmail.com>
2022-03-17 09:25:22 +0800

    建立git

---
+++ KFold_train.py
@@ -0,0 +1,351 @@
+#%%
+from functools import total_ordering
+import os
+import cv2
+import random
+
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix ,roc_curve, auc
+
+import matplotlib.pyplot as plt 
+from itertools import cycle
+import torch
+import torchvision
+from torch.utils.data import random_split, DataLoader, Subset
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+
+from model.patch_convmix_convnext import PatchConvmixConvnext
+from model.focal_loss import FocalLoss
+
+
+import wandb
+import time
+
+#%%   
+def gradCamImg(model, logPath):
+    # checkpoint = r'save.pt'
+    image_path = r'data\new_cc\correct\0a3d5f0d-6d26-4186-a4fe-7d6b2869216e_20210511_105912.png'
+    
+    # img = cv2.imread(image_path)
+    # img = cv2.resize(img, (640, 640))
+
+    torch.save(model.state_dict(), "save.pt")
+    
+    rgb_img, cam_image, cam_mask, cam_gb, gb = Grad_CAM.get_cam(model=model, image_path=image_path)
+
+
+    fig = plt.figure()
+    subplot1=fig.add_subplot(2, 2, 1)
+    subplot1.imshow(rgb_img)
+
+    subplot2=fig.add_subplot(2, 2, 2)
+    subplot2.imshow(cam_image)
+
+    subplot3=fig.add_subplot(2, 2, 3)
+    subplot3.imshow(cam_mask)
+
+    subplot4=fig.add_subplot(2, 2, 4)
+    subplot4.imshow(cam_gb)
+
+    fig.suptitle("Grad Cam of Training")
+
+    plt.savefig(logPath+"//"+"gradCam_"+str(time.strftime("%H%M%S", time.localtime()))+".jpg", bbox_inches='tight')
+    plt.close('all')
+
+def confusion(y_true, y_pred, logPath=None):
+    confmat = confusion_matrix(y_true, y_pred, labels=[0, 1])
+    fig, ax = plt.subplots(figsize=(2.5, 2.5))
+
+    ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+    for i in range(confmat.shape[0]):
+        for j in range(confmat.shape[1]):
+            ax.text(x=j, y=i, s=confmat[i,j], va='center', ha='center', fontsize=10)
+
+    TN, FP, FN, TP = confusion_matrix(y_true, y_pred, labels=[0, 1]).ravel()
+    Accuracy = (TP+TN)/(TP+FP+FN+TN)
+    # precision = TP / (TP + FP)
+    # recall = TP / (TP + FN)
+
+    if (TN + FP) == 0:
+        Specificity = 0.0
+    else:
+        Specificity = TN / (TN + FP)
+
+    if (TP + FN) == 0:
+        Sensitivity = 0.0
+    else:
+        Sensitivity = TP / (TP + FN)
+    
+    if logPath:
+        plt.xlabel('Predict', fontsize=10)        
+        plt.ylabel('True', fontsize=10)
+        
+        plt.title('Accuracy : {:.2f} | Specificity : {:.2f} | Sensitivity : {:.2f}'.format(Accuracy, Specificity, Sensitivity), fontsize=10)
+        plt.savefig(logPath + "//" + "confusion.jpg", bbox_inches='tight')
+        # plt.close('all')
+        plt.close()
+
+    return Accuracy, Specificity, Sensitivity
+
+def compute_auc(y_true, y_score, logPath=None):
+    classes = ['Ischemia', 'Infect']
+    colors = cycle(['aqua', 'darkorange'])
+
+    fpr, tpr, roc_auc = dict(), dict(), dict()
+    y_label = np.eye(len(classes))[y_true]
+    y_label = y_label.reshape(len(y_true), len(classes))
+    y_score = np.array(y_score)
+
+    if len(y_label) > 1 :
+        for i in range(len(classes)):
+            fpr[i], tpr[i], _ = roc_curve(y_label[:, i], y_score[:, i], pos_label=1)
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Plot all ROC curves
+        if logPath:
+            plt.figure()
+            
+            for i, color in zip(range(len(classes)), colors):
+                plt.plot(fpr[i], tpr[i], color=color, lw = 2,
+                        label='ROC curve of level {0} (area = {1:0.2f})'
+                        ''.format(i + 1, roc_auc[i]))
+            
+            plt.plot([0, 1], [0, 1], 'k--', lw = 2)
+            plt.xlim([0.0, 1.0])
+            plt.ylim([0.0, 1.05])
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title('multi-calss ROC')
+            plt.legend(loc="lower right")
+            plt.savefig(logPath + "//" + "compute_auc.jpg", bbox_inches='tight')
+            plt.close()
+    else:
+        roc_auc = -1.00
+
+    return roc_auc
+
+def fit_model(model, train_loader, val_loader):
+    optimizer = torch.optim.Adam(model.parameters(), lr = LR)
+    loss_func = FocalLoss(class_num=2, alpha = torch.tensor([0.5, 0.5]).cuda(), gamma = 4)
+
+    for epoch in range(EPOCH):
+        model.train()
+        training_loss = 0
+
+        for idx, (x, y) in enumerate(train_loader):
+            output = model(x.to(device))
+            outPred = output[0]
+
+            loss = loss_func(outPred, y.to(device))
+            training_loss += loss.item()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        model.eval()
+        y_pred_score =[]
+        y_true = []
+        with torch.no_grad():
+            val_loss = 0
+            for idx, (x_, y_) in enumerate(val_loader):
+                pred = model(x_.to(device))
+                loss_ = loss_func(pred, y_.to(device))
+                val_loss += loss_.item()
+
+                if type(pred) == tuple:
+                    pred = pred[0]
+
+                y_pred_score += pred.tolist()
+                y_true += y_.tolist()
+                
+            roc_auc = compute_auc(y_true, y_pred_score)
+
+        # if roc_auc > 0:
+        roc_auc = max(roc_auc.values())
+
+        if WANDBRUN:
+            wb_run.log({ 
+                        "Epoch" : epoch + 1,
+                        "Training Loss": training_loss,
+                        "Val Loss": val_loss,
+                        "Val AUC" : roc_auc,
+                    })
+            # wb_run.finish()
+            
+        print('  => Epoch : {}  Training Loss : {:.4e}  Val Loss : {:.4e}  Val AUC : {:.2}'.format(epoch + 1, training_loss, val_loss, roc_auc))
+
+        # torch.save(model.state_dict(), "last.pth")
+    return training_loss, val_loss
+
+def test_model(model, test_loader):
+    model.eval()
+    correct = 0
+    y_true = []
+    y_pred = []
+    y_pred_score = []
+
+    with torch.no_grad():
+        for idx, (x, y) in enumerate(test_loader):
+            pred = model(x.to(device))
+
+            if type(pred) == tuple:
+                pred = pred[0]
+
+            y_pred_score += pred.tolist()
+
+            pred = torch.max(pred.data, 1)[1] 
+            correct += (pred == y.to(device)).sum()
+            # print('Pred : {}, True : {}'.format(pred, y))
+
+            y_true += y.tolist()
+            y_pred += pred.tolist()
+            
+        
+        Accuracy, Specificity, Sensitivity = confusion(y_true, y_pred)
+        roc_auc = compute_auc(y_true, y_pred_score)
+
+        return Accuracy, roc_auc, Specificity, Sensitivity, y_true, y_pred, y_pred_score
+
+if __name__ == '__main__':
+    ISKFOLD = True
+    KFOLD_N = 10
+    WANDBRUN = True
+    SEED = 42
+
+    EPOCH = 20
+    BATCHSIZE = 16
+    LR = 0.0001
+
+    DATAPATH = r'C:\Data\外科溫度\裁切\已分訓練集\cut_kfold'
+
+    if SEED:
+        '''設定隨機種子碼'''
+        os.environ["PL_GLOBAL_SEED"] = str(SEED)
+        random.seed(SEED)
+        np.random.seed(SEED)
+        torch.manual_seed(SEED)
+        torch.cuda.manual_seed_all(SEED)
+
+    logPath = r"logs//" + str(time.strftime("%m%d_%H%M", time.localtime()))
+    if not os.path.isdir(logPath):
+        os.mkdir(logPath)
+
+    
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    model = PatchConvmixConvnext().to(device)
+    # if WANDBRUN:
+    #     wandb.watch(model)
+
+    transform = transforms.Compose([transforms.Resize((640, 640)),
+                                    transforms.ToTensor()])
+
+    if ISKFOLD:
+        dataset = ImageFolder(DATAPATH, transform)
+
+        kf = KFold(n_splits = KFOLD_N, shuffle = True)
+        Kfold_cnt = 0
+        acc_array = []
+        total_true = []
+        total_pred = []
+        total_pred_score = []
+        totlal_acc = 0
+
+        for train_idx, val_idx in kf.split(dataset):
+            # if WANDBRUN:
+            wb_run = wandb.init(project='infraredThermal_kfold', entity='y9760210', reinit=True, group="KFold_1", name=str("kfold_N="+str(Kfold_cnt+1)))
+
+            Kfold_cnt += 1
+
+            # if not os.path.isdir(KFoldPath):
+            #     os.mkdir(KFoldPath)
+            # print(KFoldPath)
+            
+            train = Subset(dataset, train_idx)
+            val = Subset(dataset, val_idx)
+        
+            train_loader = DataLoader(train, batch_size = BATCHSIZE, shuffle = True, num_workers = 2)
+            val_loader = DataLoader(val, batch_size = BATCHSIZE, shuffle = True, num_workers = 2)
+            
+            fit_model(model, train_loader, val_loader)
+
+            Accuracy, roc_auc, Specificity, Sensitivity, kfold_true, kfold_pred,  kfold_pred_score = test_model(model, val_loader)
+            totlal_acc += Accuracy
+            total_true += kfold_true
+            total_pred += kfold_pred
+            total_pred_score += kfold_pred_score
+            # if roc_auc > 0:
+            roc_auc = max(roc_auc.values())
+
+            if WANDBRUN:
+                wb_run.log({
+                            "Accuracy" : Accuracy,
+                            "Test AUC" : roc_auc,
+                            "Test Specificity" : Specificity,
+                            "Test Sensitivity" : Sensitivity
+                        })
+                # wb_run.finish()
+
+            print("===================================================================================================")
+            print('Kfold : {} , Accuracy : {:.2e} , Test AUC : {:.2} , Specificity : {:.2} , Sensitivity : {:.2}'.format(Kfold_cnt, Accuracy, roc_auc, Specificity, Sensitivity))
+            print("===================================================================================================")
+        
+        Accuracy, Specificity, Sensitivity = confusion(total_true, total_pred, logPath)
+        roc_auc = compute_auc(total_true, total_pred_score, logPath)
+        print("===================================================================================================")
+        print('Kfold:N= : {} ,Total = Accuracy : {:.3} , Specificity : {:.2} , Sensitivity : {:.2}'.format(KFOLD_N, Accuracy, Specificity, Sensitivity))
+        print("Total AUC: ", roc_auc)
+        print("===================================================================================================")
+
+        roc_auc = max(roc_auc.values())
+        if WANDBRUN:
+            wb_run.log({
+                        "KFold Accuracy" : Accuracy,
+                        "KFold AUC" : roc_auc,
+                        "KFold Specificity" : Specificity,
+                        "KFold Sensitivity" : Sensitivity
+                    })
+            wb_run.finish()
+
+    if WANDBRUN:
+        wb_run.finish()
+    #     acc_array.append(accuracy)
+
+    #     # print("Accuracy :", accuracy)
+    #     # print("==============================================")
+
+        
+    #     print('KFold-{} : Accuracy = {}'.format(cnt, accuracy))
+    #     print('================================')
+    #     if WANDBRUN:
+    #         wandb.log({"Accuracy": accuracy})
+    #         wb_run.finish()
+    # else:
+    #     train_dataset = ImageFolder(DATAPATH + r'\train', transform)
+    #     val_dataset = ImageFolder(DATAPATH + r'\val', transform)
+    #     test_dataset = ImageFolder(DATAPATH + r'\test', transform)
+
+    #     train_loader = DataLoader(train_dataset, batch_size = BATCHSIZE, shuffle = True, num_workers = 2)
+    #     val_loader = DataLoader(val_dataset, batch_size = BATCHSIZE, shuffle = True, num_workers = 2)
+    #     test_loader = DataLoader(test_dataset, shuffle = True)
+
+    #     fit_model(model, train_loader, val_loader, logPath)
+    #     accuracy = FinalTest(model, test_loader, logPath)
+        
+#%%
+# wb_run = wandb.init(project='infraredThermal_kfold', entity='y9760210', reinit=True, group="KFold_1", name=str("kfold_N="+str(2)))
+# wb_run.finish()
+
+
+#%%
+# from PIL import Image
+# for idx in test_idx:
+#     to_PIL = transforms.ToPILImage()
+#     img = dataset.__getitem__(idx)[0]
+#     img = to_PIL(img)
+#     img.show()
+#     print(idx, dataset.__getitem__(idx)[1])
+
---
+++ model/patch_convmix_convnext.py
@@ -0,0 +1,233 @@
+#%%
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(x) + x
+
+def convmixer_layer(dim, depth, kernel_size):
+    return nn.Sequential(   # 7x7+1x1
+        *[nn.Sequential(
+                Residual(nn.Sequential(
+                    Residual(
+                        nn.Sequential(
+                            nn.Conv2d(dim, dim, kernel_size, groups=dim, padding = 3),
+                            nn.GELU(),
+                            nn.BatchNorm2d(dim),
+                        )),
+
+                    nn.Conv2d(dim, dim, kernel_size=3, padding = 1),
+                    nn.GELU(),
+                    nn.BatchNorm2d(dim),
+                )),
+                
+        # )
+        ) for i in range(depth)],
+    )
+    ''' 
+    return nn.Sequential(   # 7x7+3x3+1x1
+        *[Residual(nn.Sequential(
+                Residual(nn.Sequential(
+                    Residual(nn.Sequential(
+                                nn.Conv2d(in_channels = dim, out_channels = dim, kernel_size = kernel_size, groups = dim, padding = 3),
+                                nn.GELU(),
+                            )),
+                    nn.Conv2d(dim, dim, kernel_size=3, padding = 1),
+                    nn.GELU(),
+                    )),
+                nn.Conv2d(dim, dim, kernel_size=1),
+                nn.GELU(),
+                
+        )) for i in range(depth)],
+    )
+    '''
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+class Block(nn.Module):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    We use (2) as we find it slightly faster in PyTorch
+    
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
+        # self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.drop_path = nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
+
+
+
+class PatchConvmixConvnext(nn.Module):
+    def __init__(self, dim = 768, depth = 3, kernel_size = 7, patch_size = 16, n_classes = 2):
+        super(PatchConvmixConvnext, self).__init__()
+        self.dim = dim
+        self.depth = depth
+        self.n_class = n_classes
+        # self.inside_dim = inside_dim
+        self.kernel_size = kernel_size
+
+        self.patch_embed = nn.Sequential(
+            nn.Conv2d(3, dim, kernel_size = patch_size, stride = patch_size),
+            nn.GELU(),
+            # nn.LeakyReLU(),
+            nn.BatchNorm2d(dim)
+        )
+
+        self.cm_layer = convmixer_layer(self.dim, self.depth, self.kernel_size)
+
+        # self.convNeXt = nn.Sequential(
+        #     *[Block(dim=dim) for i in range(3)]
+        # )
+
+        # self.gap = nn.AdaptiveAvgPool2d((1,1))
+        # self.flat = nn.Flatten()
+
+        self.fc1 = nn.Linear(dim, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, n_classes)
+
+        # self.norm = nn.LayerNorm(dim, eps=1e-6) # final norm layer
+        # self.head = nn.Linear(dim, n_classes)
+    
+    def forward(self, x):
+        x = self.patch_embed(x)
+        x = self.cm_layer(x)
+
+        # x = self.convNeXt(x)
+
+        gap = torch.mean(x,1)       # 可視化層
+
+        # x = self.gap(x)
+        # x = self.norm(x.mean([-2, -1]))             # global average pooling, (N, C, H, W) -> (N, C)
+        x = x.mean([-2, -1])
+        # x = self.flat(x)
+
+        x = self.fc1(x)
+        x = self.fc2(x)
+        output = self.fc3(x)
+        # output = self.head(x)
+        return output, gap
+
+if __name__ == '__main__':
+    import torch
+    import netron
+    import torch.optim as optim
+
+    # GPU
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    print('GPU State:', device)
+    model = PatchConvmixConvnext(dim = 768, depth = 4, kernel_size = 7, patch_size = 16, n_classes = 2)
+    # print(model)
+
+    heat_visual = False
+
+    if heat_visual:
+        from cgi import test
+        import cv2
+        import torch
+        from torchvision import transforms
+        from torch.utils.data import DataLoader
+        from torchvision.datasets import ImageFolder
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        WEIGHTPATH = r'G:\我的雲端硬碟\Lab\Project\外科溫度\infraredthermal_PL\log\best_check_point\best.pth'
+        DATAPATH = r"G:\我的雲端硬碟\Lab\Project\外科溫度\infraredthermal_PL\log\輸入測試\\"
+        # SAVEPATH = r'G:\我的雲端硬碟\Lab\Project\外科溫度\infraredthermal_PL\log\patchimg'
+        SAVEPATH = None
+
+        transform = transforms.Compose([transforms.Resize((640, 640)),
+                                    transforms.ToTensor(),])
+
+
+        model.eval()
+        model.load_state_dict(torch.load(WEIGHTPATH))
+        model.to(device)
+
+
+        test = ImageFolder(DATAPATH + 'test', transform)
+        test_loader = DataLoader(test, shuffle = False)
+        
+        for idx, (x, y) in enumerate(test_loader):
+            # cam_visualize(idx, model, x.to(device))
+            x = model.patch_embed(x.to(device))
+            x = x[0].permute(0, 1, 2).to('cpu').detach().numpy()
+            for i in range(x.shape[0]):
+                # plt.imshow(x[i])
+                if SAVEPATH:
+                    plt.imsave(SAVEPATH + '\\' + str(i) + '.jpg', x[i])
+                plt.show()
+
+    else:
+        _input = torch.randn(1, 3, 640, 640)
+        s = model(_input)
+
+        # print(s[1][0].detach().numpy().shape)
+        # plt.imshow(s[1][0].detach().numpy())        # 顯示特徵萃取效果
+        # plt.show()
+
+        # _input = _input.unsqueeze(0)
+        # # print(_input.size())
+        onxx_path = r'model.onnx'
+        torch.onnx.export(model, _input, onxx_path)
+
+        netron.start(onxx_path)
