5902def891f00b15177cd3d7409e8765d1a37eec
KNChiu <y9760210@gmail.com>
2022-03-20 13:15:11 +0800

    移除未使用功能

--- model/patch_convmix_convnext.py
+++ model/patch_convmix_convnext.py
@@ -13,7 +13,7 @@ class Residual(nn.Module):
         return self.fn(x) + x
 
 def convmixer_layer(dim, depth, kernel_size):
-    return nn.Sequential(   # 7x7+1x1
+    return nn.Sequential(   # 7x7+3x3
         *[nn.Sequential(
                 Residual(nn.Sequential(
                     Residual(
@@ -48,72 +48,6 @@ def convmixer_layer(dim, depth, kernel_size):
         )) for i in range(depth)],
     )
     '''
-
-# class LayerNorm(nn.Module):
-#     r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
-#     The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
-#     shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
-#     with shape (batch_size, channels, height, width).
-#     """
-#     def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
-#         super().__init__()
-#         self.weight = nn.Parameter(torch.ones(normalized_shape))
-#         self.bias = nn.Parameter(torch.zeros(normalized_shape))
-#         self.eps = eps
-#         self.data_format = data_format
-#         if self.data_format not in ["channels_last", "channels_first"]:
-#             raise NotImplementedError 
-#         self.normalized_shape = (normalized_shape, )
-    
-#     def forward(self, x):
-#         if self.data_format == "channels_last":
-#             return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
-#         elif self.data_format == "channels_first":
-#             u = x.mean(1, keepdim=True)
-#             s = (x - u).pow(2).mean(1, keepdim=True)
-#             x = (x - u) / torch.sqrt(s + self.eps)
-#             x = self.weight[:, None, None] * x + self.bias[:, None, None]
-#             return x
-
-# class Block(nn.Module):
-#     r""" ConvNeXt Block. There are two equivalent implementations:
-#     (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
-#     (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
-#     We use (2) as we find it slightly faster in PyTorch
-    
-#     Args:
-#         dim (int): Number of input channels.
-#         drop_path (float): Stochastic depth rate. Default: 0.0
-#         layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
-#     """
-#     def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
-#         super().__init__()
-#         self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
-#         self.norm = LayerNorm(dim, eps=1e-6)
-#         self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
-#         self.act = nn.GELU()
-#         self.pwconv2 = nn.Linear(4 * dim, dim)
-#         self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
-#                                     requires_grad=True) if layer_scale_init_value > 0 else None
-#         # self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-#         self.drop_path = nn.Identity()
-
-#     def forward(self, x):
-#         input = x
-#         x = self.dwconv(x)
-#         x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
-#         x = self.norm(x)
-#         x = self.pwconv1(x)
-#         x = self.act(x)
-#         x = self.pwconv2(x)
-#         if self.gamma is not None:
-#             x = self.gamma * x
-#         x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
-
-#         x = input + self.drop_path(x)
-#         return x
-
-
 
 class PatchConvmixConvnext(nn.Module):
     def __init__(self, dim = 768, depth = 3, kernel_size = 7, patch_size = 16, n_classes = 2):
@@ -150,18 +84,13 @@ class PatchConvmixConvnext(nn.Module):
     def forward(self, x):
         x = self.patch_embed(x)
         x = self.cm_layer(x)
-        # x = self.convNeXt(x)
         gap = torch.mean(x,1)       # 可視化層
-        # x = self.gap(x)
-        # x = self.norm(x.mean([-2, -1]))             # global average pooling, (N, C, H, W) -> (N, C)
         x = x.mean([-2, -1])
         featureOut = x
-        # x = self.flat(x)
 
         x = self.fc1(x)
         x = self.fc2(x)
         output = self.fc3(x)
-        # output = self.head(x)
         return output, gap, featureOut
 
 if __name__ == '__main__':
