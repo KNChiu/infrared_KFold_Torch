f727177044d6ea7caa78ea824b94ab3582dd5a70
KNChiu <y9760210@gmail.com>
2022-03-25 00:23:58 +0800

    修改函示內變數名稱錯誤

--- model/assessment_tool.py
+++ model/assessment_tool.py
@@ -23,13 +23,31 @@ class MyEstimator():
         # plt.close('all')
         return img
 
-    def confusion(self, y_true, y_pred, calsses, logPath=None, mode = ''):
+    def confusion(self, y_true, y_pred, val_keyLabel=None, classes=2, logPath=None, mode = ''):
         Accuracy = accuracy_score(y_true, y_pred)
         Specificity = 0.0
         Sensitivity = 0.0
         plot_img_np = None
 
-        if len(calsses) == 2:
+        y_true = np.array(y_true)
+        y_pred = np.array(y_pred)
+
+        error_list = {'1_to_0':[], '0_to_1':[]}
+        t1p0 = []
+        t0p1 = []
+        if val_keyLabel:
+            for i in range(len(val_keyLabel)):
+
+                if y_true[i] == 1 and y_pred[i] == 0:
+                    t1p0.append(val_keyLabel[i])
+
+                elif y_true[i] == 0 and y_pred[i] == 1:
+                    t0p1.append(val_keyLabel[i])
+
+        error_list['1_to_0'] = t1p0
+        error_list['0_to_1'] = t0p1
+        
+        if len(classes) == 2:
             cf_matrix = confusion_matrix(y_true, y_pred, labels=[0, 1])
             TN, FP, FN, TP = confusion_matrix(y_true, y_pred, labels=[0, 1]).ravel()
             if (TN + FP) != 0:
@@ -41,7 +59,7 @@ class MyEstimator():
 
         if logPath:
             plt.close("all")
-            df_cm = pd.DataFrame(cf_matrix, calsses, calsses)
+            df_cm = pd.DataFrame(cf_matrix, classes, classes)
             plt.figure(figsize = (9,6))
             sns.heatmap(df_cm, annot=True, fmt="d", cmap='BuGn')
             plt.xlabel('Predict', fontsize=10)        
@@ -52,7 +70,8 @@ class MyEstimator():
             plt.savefig(logPath + "//" + str(mode) + "_confusion.jpg", bbox_inches='tight')
             plt.close('all')
 
-        return Accuracy, Specificity, Sensitivity, plot_img_np
+
+        return Accuracy, Specificity, Sensitivity, error_list, plot_img_np
 
     def compute_auc(self, y_true, y_score, classes, logPath=None, mode = ''):
         colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
